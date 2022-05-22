0dec8f7423a94175d0f348cba6b042caa494a51e
sulmeee <poorooria@naver.com>
2022-05-22 18:02:03 +0900

    make add discussion.js

--- data.js
+++ data.js
@@ -1,11 +1,9 @@
 const agoraStatesDiscussions = [
-    {
-        id: "kimdasol1234",
-        createdAt: "2022-05-15 23:57:43",
-        chat:
-          "반갑습니다",
-        author: "kimdasol",
-        avatarUrl:
-          "https://avatars.githubusercontent.com/u/90553688?s=64&u=3c4e4dc2053d4977ac12b9cfc2667582f986d3d8&v=4"
-      }
+  {
+    id: "kimdasol1234",
+    avatarUrl:"https://avatars.githubusercontent.com/u/77476348?s=64&u=64243db62117de5c254c9a76184753b76d7303ff&v=4",
+    chat:"반갑습니다",
+    author: "kimdasol",
+    createdAt: "2022-05-15 23:57:43"
+    }
 ]
--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const convertToDiscussion = (obj) => {
 
     //// discussion__avatar--wrapper
     const avatarWrapper = document.createElement("div"); // div 요소 생성
-    avatarWrapper.className = "discussion__avatar--wrapper"; 
+    avatarWrapper.className = "discussion__avatar--wrapper";
 
     // discussion__avatar--image
     const avatarImg = document.createElement('img');
@@ -27,13 +27,13 @@ const convertToDiscussion = (obj) => {
     const discussionChat = document.createElement("p");
     discussionChat.className = "discussion__chat";
     discussionChat.textContent =
-    `${obj.chat}`;
+        `${obj.chat}`;
 
     // discussion__information
     const discussionInformation = document.createElement("div");
     discussionInformation.className = "discussion__information";
-    discussionInformation.textContent = 
-    `${obj.author} / ${new Date(obj.createdAt).toLocaleTimeString()}`;
+    discussionInformation.textContent =
+        `${obj.author} / ${new Date(obj.createdAt).toLocaleTimeString()}`;
     discussionContent.append(discussionChat, discussionInformation);
 
     //  li요소에 모든 하위요소 추가
@@ -44,11 +44,46 @@ const convertToDiscussion = (obj) => {
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
     for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-      element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+        element.append(convertToDiscussion(agoraStatesDiscussions[i]));
     }
     return;
-  };
-  
-  // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
-  const ul = document.querySelector("ul.discussions__container");
-  render(ul);
+};
+
+// ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
+const ul = document.querySelector("ul.discussions__container");
+render(ul);
+
+
+//// Bare Minimum Requirement
+// 디스커션 추가 기능
+// 아이디, 내용 입력 후 버튼을 눌렀을 때 화면에 디스커션 추가
+// 배열에 추가한 데이터 쌓기
+
+// add data
+const submit = document.querySelector('.submit');
+const Name = document.querySelector('#name');
+const Story = document.querySelector('#story');
+
+// event
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let newDiscussion = {
+        id : Name.value,
+        avatarUrl : "https://avatars.githubusercontent.com/u/92300124?s=64&u=82d8e12bfca4deb05c7f86db7d3c15e06ca34d05&v=4",
+        chat : Story.value,
+        author : Name.value,
+        createdAt : new Date().toISOString()
+    };
+
+    console.log(newDiscussion);
+    agoraStatesDiscussions.unshift(newDiscussion);
+    console.log(agoraStatesDiscussions);
+
+    ul.prepend(convertToDiscussion(newDiscussion));
+
+    Name.value = ""; // 왜 값을 빈문자열로 변경했을까?
+    Story.value = "";
+
+    return false;
+});
